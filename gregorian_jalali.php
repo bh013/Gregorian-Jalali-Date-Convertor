844205b15541bbc7031b5705c0135deee10ffa69
bh013 <qwer@hi2.in>
2017-07-18 16:54:44 +0100

    Update gregorian_jalali.php

--- gregorian_jalali.php
+++ gregorian_jalali.php
@@ -1,134 +1,58 @@
-<?php 
-
- /*
- * @method gregorian_to_jalali ($g_y, $g_m, $g_d,$str)       return gregorian to jalali converted date 
- * @method jalali_to_gregorian($j_y, $j_m, $j_d,$str)        return jalali to gregorian converted date 
- * 
- * @author     Roozbeh Baabakaan
- * @version    SVN: $Id: gregorian_jalali 1 2012-12-20 19:07:0 
- */
- 
-function div($a,$b) { 
-    return (int) ($a / $b); 
+<?php
+$gdim=[31,28,31,30,31,30,31,31,30,31,30,31];
+$jdim=[31,31,31,31,31,31,30,30,30,30,30,29];
+function div($a,$b){return (int)($a/$b);}
+function g2j($gy,$gm,$gd,$str=1){
+	global $gdim,$jdim;$gy-=1600;--$gm;--$gd;
+	$gdn=365*$gy+div($gy+3,4)-div($gy+99,100)+div($gy+399,400);
+	for($i=0;$i<$gm;++$i)
+		$gdn+=$gdim[$i];
+	if($gm>1&&(($gy%4==0&&$gy%100)||!($gy%400))) $gdn++;
+	$gdn+=$gd;
+	$jdn=$gdn-79;
+	$j_np=div($jdn,12053);
+	$jdn%=12053;
+	$jy=979+33*$j_np+4*div($jdn,1461);
+	$jdn%=1461;
+	if($jdn>365){$jy+=div($jdn-1,365);$jdn=($jdn-1)%365;}
+	for($i=0;$i<11&&$jdn>=$jdim[$i];++$i)
+		$jdn-=$jdim[$i];
+	$jm=$i+1;
+	$jd=$jdn+1;
+	return $str?$jy.'/'.$jm.'/'.$jd:[$jy,$jm,$jd];
+}
+function j2g($jy,$jm,$jd,$str=1){
+	global $gdim,$jdim;$jy-=979;--$jm;--$jd;
+	$jdn=365*$jy+div($jy,33)*8+div($jy%33+3,4);
+	for($i=0;$i<$jm;++$i)
+		$jdn+=$jdim[$i];
+	$jdn+=$jd;
+	$gdn=$jdn+79;
+	$gy=1600+400*div($gdn,146097);
+	$gdn%=146097;
+	$leap=1;
+	if($gdn>36524){
+		$gy+=100*div(--$gdn,36524);
+		$gdn%=36524;
+		if($gdn>364) $gdn++;
+		else $leap=0;}
+	$gy+=4*div($gdn,1461);
+	$gdn%=1461;
+	if($gdn>365){
+		$gy+=div(--$gdn,365);
+		$gdn%=365;
+		$leap=0;} 
+	for($i=0;$gdn>=($Q=$gdim[$i]+($i==1&&$leap));$i++)
+		$gdn-=$Q;
+	$gm=$i+1;
+	$gd=$gdn+1;
+	return $str?$gy.'/'.$gm.'/'.$gd:[$gy,$gm,$gd];
 } 
- 
-function gregorian_to_jalali ($g_y, $g_m, $g_d,$str) 
-{ 
-    $g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31); 
-    $j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29); 
- 
-  
-   $gy = $g_y-1600; 
-   $gm = $g_m-1; 
-   $gd = $g_d-1; 
- 
-   $g_day_no = 365*$gy+div($gy+3,4)-div($gy+99,100)+div($gy+399,400); 
- 
-   for ($i=0; $i < $gm; ++$i) 
-      $g_day_no += $g_days_in_month[$i]; 
-   if ($gm>1 && (($gy%4==0 && $gy%100!=0) || ($gy%400==0))) 
-      /* leap and after Feb */ 
-      $g_day_no++; 
-   $g_day_no += $gd; 
- 
-   $j_day_no = $g_day_no-79; 
- 
-   $j_np = div($j_day_no, 12053); /* 12053 = 365*33 + 32/4 */ 
-   $j_day_no = $j_day_no % 12053; 
- 
-   $jy = 979+33*$j_np+4*div($j_day_no,1461); /* 1461 = 365*4 + 4/4 */ 
- 
-   $j_day_no %= 1461; 
- 
-   if ($j_day_no >= 366) { 
-      $jy += div($j_day_no-1, 365); 
-      $j_day_no = ($j_day_no-1)%365; 
-   } 
- 
-   for ($i = 0; $i < 11 && $j_day_no >= $j_days_in_month[$i]; ++$i) 
-      $j_day_no -= $j_days_in_month[$i]; 
-   $jm = $i+1; 
-   $jd = $j_day_no+1; 
- if($str) return $jy.'/'.$jm.'/'.$jd ;
-   return array($jy, $jm, $jd); 
-} 
- 
-function jalali_to_gregorian($j_y, $j_m, $j_d,$str) 
-{ 
-    $g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31); 
-    $j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29); 
- 
- 
-   $jy = (int)($j_y)-979; 
-   $jm = (int)($j_m)-1; 
-   $jd = (int)($j_d)-1; 
- 
-   $j_day_no = 365*$jy + div($jy, 33)*8 + div($jy%33+3, 4); 
-   
-   for ($i=0; $i < $jm; ++$i) 
-      $j_day_no += $j_days_in_month[$i]; 
- 
-   $j_day_no += $jd; 
- 
-   $g_day_no = $j_day_no+79; 
- 
-   $gy = 1600 + 400*div($g_day_no, 146097); /* 146097 = 365*400 + 400/4 - 400/100 + 400/400 */ 
-   $g_day_no = $g_day_no % 146097; 
- 
-   $leap = true; 
-   if ($g_day_no >= 36525) /* 36525 = 365*100 + 100/4 */ 
-   { 
-      $g_day_no--; 
-      $gy += 100*div($g_day_no,  36524); /* 36524 = 365*100 + 100/4 - 100/100 */ 
-      $g_day_no = $g_day_no % 36524; 
- 
-      if ($g_day_no >= 365) 
-         $g_day_no++; 
-      else 
-         $leap = false; 
-   } 
- 
-   $gy += 4*div($g_day_no, 1461); /* 1461 = 365*4 + 4/4 */ 
-   $g_day_no %= 1461; 
- 
-   if ($g_day_no >= 366) { 
-      $leap = false; 
- 
-      $g_day_no--; 
-      $gy += div($g_day_no, 365); 
-      $g_day_no = $g_day_no % 365; 
-   } 
- 
-   for ($i = 0; $g_day_no >= $g_days_in_month[$i] + ($i == 1 && $leap); $i++) 
-      $g_day_no -= $g_days_in_month[$i] + ($i == 1 && $leap); 
-   $gm = $i+1; 
-   $gd = $g_day_no+1; 
-    if($str) return $gy.'/'.$gm.'/'.$gd ;
-    return array($gy, $gm, $gd); 
-} 
-
-
-function comparedate($_date_mix_jalaly,$_date_mix_gregorian)
-{
-  $_date_arr_jalaly = explode('/', $_date_mix_jalaly);
-  $_date_arr_gregorian = explode('/', $_date_mix_gregorian);		
-  
-  $arr_jtg = jalali_to_gregorian($_date_arr_jalaly[0],$_date_arr_jalaly[1],$_date_arr_jalaly[2]);
-		
-  if($_date_arr_gregorian[0]> $arr_jtg[0])
-    {
-	 return  false;
-	}
-		
-	else if($_date_arr_gregorian[0]== $arr_jtg[0] && $_date_arr_gregorian[1]>$arr_jtg[1])
-	{
-	 return false;
-	}
-	else if($_date_arr_gregorian[0]== $arr_jtg[0] && $_date_arr_gregorian[1]==$arr_jtg[1] && $_date_arr_gregorian[2]>$arr_jtg[2])
-	{
-	 return false;
-	}
-  return true ;	
+function cmp($j,$g){
+	$J=explode('/',$j);$G=explode('/',$g);$arr=j2g($J[0],$J[1],$J[2]);
+	if($G[0]>$arr[0]) return false;
+	if($G[0]==$arr[0]&&$G[1]>$arr[1]) return false;
+	if($G[0]==$arr[0]&&$G[1]==$arr[1]&&$G[2]>$arr[2]) return false;return true;	
 }
 
 ?>
